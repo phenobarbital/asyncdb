41b8db07144fe48a05cf83b480cf0f18cd7bd454
Jesus Lara <jesuslarag@gmail.com>
2019-08-14 02:48:43 +0200

    setup project package

---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""AsyncDB
+    Asynchronous library for data source connections, used by Navigator.
+See:
+https://bitbucket.org/mobileinsight1/asyncdb/src/master/
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='asyncdb',
+    version='1.0.0',
+    url='https://bitbucket.org/mobileinsight1/asyncdb/',
+    description='Asyncio Datasource library',
+    long_description='Asynchronous library for data source connections, used by Navigator',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'Programming Language :: Python :: 3.7',
+    ],
+    author='Jesus Lara',
+    author_email='jlara@trocglobal.com',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=['numpy >= 1.11.1', 'asyncio==3.4.3'],
+    project_urls={  # Optional
+        'Source': 'https://bitbucket.org/mobileinsight1/asyncdb/',
+        'Funding': 'https://paypal.me/phenobarbital',
+        'Say Thanks!': 'https://saythanks.io/to/phenobarbital',
+    },
+)
