5fdadc2a1539bb76b333c0ee6477497a9fa78a79
Jesus Lara <jesuslarag@gmail.com>
2019-09-06 19:22:09 +0200

    debug on logging.INFO

--- asyncdb/__init__.py
+++ asyncdb/__init__.py
@@ -10,7 +10,7 @@ from asyncdb.providers import *
 from asyncdb.meta import asyncORM, asyncRecord
 
 
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 logger.debug("Running AsyncDB version %s" % __version__)
