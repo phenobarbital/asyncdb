a70bf14243c2a33b3f8050c20541e89cf936e963
Jesus Lara <jesuslarag@gmail.com>
2019-09-14 16:32:47 +0200

    test redis

--- test_redis.py
+++ test_redis.py
@@ -3,8 +3,11 @@ import asyncio
 loop = asyncio.get_event_loop()
 asyncio.set_event_loop(loop)
 
-from asyncdb import AsyncDB
-from asyncdb.exceptions import ProviderError, NoDataFound
+# from asyncdb import AsyncDB
+# from asyncdb.providers.redis import redis
+#
+# from asyncdb.exceptions import ProviderError, NoDataFound
 
 redis_url = 'redis://localhost:6379/3'
-r = AsyncDB('redis', dsn=redis_url, loop=loop)
+# r = AsyncDB('redis', dsn=redis_url, loop=loop)
+# rd = redis(dns=redis_url, loop=loop)
