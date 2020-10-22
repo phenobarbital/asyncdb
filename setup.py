18e48b2e2b432a5902b6d44030e3be77ac5eb9f5
phenobarbital <jesuslarag@gmail.com>
2020-10-22 19:27:10 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 """AsyncDB
     Asynchronous library for data source connections, used by Navigator.
 See:
-https://bitbucket.org/mobileinsight1/asyncdb/src/master/
+https://github.com/phenobarbital/asyncdb
 """
 
 from setuptools import find_packages, setup
@@ -10,7 +10,7 @@ from setuptools import find_packages, setup
 setup(
     name="asyncdb",
     version="1.0.0",
-    url="https://bitbucket.org/mobileinsight1/asyncdb/",
+    url="https://github.com/phenobarbital/asyncdb",
     description="Asyncio Datasource library",
     long_description="Asynchronous library for data source connections, used by Navigator",
     classifiers=[
@@ -22,9 +22,42 @@ setup(
     author="Jesus Lara",
     author_email="jlara@trocglobal.com",
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
-    install_requires=["numpy >= 1.11.1", "asyncio==3.4.3", "python-dateutil==2.8.1"],
+    install_requires=[
+        'numpy >= 1.11.1',
+        'asyncio==3.4.3',
+        'python-dotenv==0.14.0',
+        'PyDrive==1.3.1',
+        'uvloop==0.14.0',
+        'objectpath==0.6.1',
+        'MarkupSafe==1.1.1',
+        'jinja2==2.11.2',
+        'jsonpath-rw==1.4.0',
+        'jsonpath-rw-ext==1.2.2',
+        'dateparser==0.7.6',
+        'iso8601==0.1.13',
+        'aiopg==1.0.0',
+        'tqdm==4.50.2',
+        'tabulate==0.8.7',
+        'python-magic==0.4.18',
+        'cryptography==3.1.1',
+        'pgpy==0.5.3',
+        'botocore==1.18.18',
+        'boto3==1.15.18',
+        'xlrd==1.2.0',
+        'bs4==0.0.1',
+        'pylibmc==1.6.1',
+        'redis==3.5.3',
+        'aioredis==1.3.1',
+        'asyncpg==0.21.0',
+        'rethinkdb==2.4.7',
+        'sqlalchemy==1.3.20',
+        'openpyxl==3.0.5',
+        'zeep==4.0.0',
+        'lxml==4.6.0',
+        'isodate==0.6.0'
+    ],
     project_urls={  # Optional
-        "Source": "https://bitbucket.org/mobileinsight1/asyncdb/",
+        "Source": "https://github.com/phenobarbital/asyncdb",
         "Funding": "https://paypal.me/phenobarbital",
         "Say Thanks!": "https://saythanks.io/to/phenobarbital",
     },
