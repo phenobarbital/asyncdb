74d6f27ace10305cb63641ebcb27023987d68676
phenobarbital <jesuslarag@gmail.com>
2020-10-23 01:08:24 +0200

    centralizing all useful functions

--- asyncdb/utils/__init__.py
+++ asyncdb/utils/__init__.py
@@ -1,3 +1,4 @@
 from .functions import SafeDict, is_udf, is_pgconstant
 from .encoders import DefaultEncoder, EnumEncoder
+
 __all__ = [ 'SafeDict', 'DefaultEncoder', 'EnumEncoder', 'is_pgconstant', 'is_udf' ]
