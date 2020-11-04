3f81b7dddcd96fe8d13b5d7eb24b31d9982a3c61
Jesus Lara <jesuslarag@gmail.com>
2020-11-04 21:05:41 +0100

    Working on: Fix some RethinkDb troubles on component

---
+++ tests/test_rethink.py
@@ -0,0 +1,41 @@
+import asyncio
+import logging
+
+loop = asyncio.get_event_loop()
+asyncio.set_event_loop(loop)
+loop.set_debug(True)
+
+logging.basicConfig(level=logging.INFO, format="%(name)s - %(levelname)s - %(message)s")
+logger = logging.getLogger(__name__)
+
+
+from asyncdb import AsyncDB, AsyncPool
+from asyncdb.exceptions import NoDataFound, ProviderError
+from asyncdb.providers.rethink import rethink
+
+
+params = {
+    "host": "localhost",
+    "port": "28015",
+    "db": "troc"
+}
+
+rt = AsyncDB("rethink", params=params, loop=loop)
+
+async def connect(db):
+    async with await db.connection() as conn:
+        print("Connected: {}".format(conn.is_connected))
+        print(await conn.test_connection())
+        conn.use('troc')
+        db = await conn.list_databases()
+        print(db)
+        filter = {"company_id": 10, "state_code": "FL"}
+        result, error = await conn.query('troc_populartimes', filter=filter)
+        if not error:
+            for row in result:
+                print(row)
+try:
+    loop.run_until_complete(connect(rt))
+finally:
+    loop.run_until_complete(rt.close())
+    loop.close()
