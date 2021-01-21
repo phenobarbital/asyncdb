456da4dfff0831e8010d143ad5df8ec1ec1cdeed
phenobarbital <jesuslarag@gmail.com>
2021-01-21 17:00:57 +0100

    MakeModel solution

--- probe/test_model.py
+++ probe/test_model.py
@@ -26,24 +26,26 @@ args = {
 }
 p = AsyncDB("pg", params=params, **args)
 
-# f = [
-#     ('sara_order_no', str),
-#     ('dealer_name', str),
-#     ('dealer_code', str),
-#     ('retailer', str),
-#     ('store_no', int),
-#     ('account_number', np.int64),
-#     ('lead_rep_name', Decimal),
-#     ('activity_date', datetime.datetime)
-# ]
-#
-# act = Model.make_model(name='activity_data', schema='att', fields=f)
-# m = act()
-# print(m.schema(type='json'))
+f = [
+    ('sara_order_no', str),
+    ('dealer_name', str),
+    ('dealer_code', str),
+    ('retailer', str),
+    ('store_no', int),
+    ('account_number', np.int64),
+    ('lead_rep_name', Decimal),
+    ('activity_date', datetime.datetime)
+]
+
 async def create_model():
     query = await Model.makeModel(name='query_util', schema='troc', db=p)
     q = await query.filter(query_slug='walmart_stores')
     print(q, q[0].query_raw)
+    print(q[0].schema(type='sql'))
+    # also, another type:
+    act = await Model.makeModel(name='activity_data', schema='att', fields=f, db=p)
+    m = act()
+    print(m.schema(type='sql'))
 
 if __name__ == '__main__':
     loop.run_until_complete(create_model())
