7155e5d09e64661f751a67cb7f878c2798500215
phenobarbital <jesuslarag@gmail.com>
2021-01-21 17:00:57 +0100

    MakeModel solution

--- asyncdb/utils/models.py
+++ asyncdb/utils/models.py
@@ -64,6 +64,33 @@ DB_TYPES = {
     List: 'jsonb'
 }
 
+MODEL_TYPES = {
+ "boolean": bool,
+ "integer": int,
+ "bigint": np.int64,
+ "float": float,
+ "character varying": str,
+ "string": str,
+ "varchar": str,
+ "byte": bytes,
+ "bytea": bytes,
+ "Array": list,
+ "hstore": dict,
+ "character varying[]": list,
+ "numeric": Decimal,
+ "date": datetime.date,
+ "timestamp with time zone": datetime.datetime,
+ "time": datetime.time,
+ "timestamp without time zone": datetime.timedelta,
+ "uuid": uuid.UUID,
+ "json": dict,
+ "jsonb": dict,
+ "text": str,
+ "serial": int,
+ "bigserial": int,
+ "inet": str
+}
+
 JSON_TYPES = {
     bool: "boolean",
     int: "integer",
@@ -965,12 +992,18 @@ class Model(metaclass=ModelMeta):
             colinfo = await db.column_info(tablename)
             fields = []
             for column in colinfo:
+                tp = column['data_type']
                 col = Field(
                     primary_key=column['is_primary'],
                     notnull=column['notnull'],
-                    db_type=column['data_type']
+                    db_type=column['format_type']
                 )
-                fields.append((column['column_name'], str, col))
+                # get dtype from database type:
+                try:
+                    dtype = MODEL_TYPES[tp]
+                except KeyError:
+                    dtype = str
+                fields.append((column['column_name'], dtype, col))
         cls = make_dataclass(name, fields, bases=(Model,))
         m = Meta()
         m.name = name
