2ea7e66487106f12474e584c7d7bb958e2692ea1
Jesus Lara <jesuslarag@gmail.com>
2021-05-27 01:59:39 +0200

    Cassandra basics without cursors or other stuff

--- probe/test_cassandra.py
+++ probe/test_cassandra.py
@@ -21,6 +21,29 @@ async def test_connect(driver, params, event_loop):
     result, error = await db.test_connection()
     print(result, error)
     print(type(result) == list)
+    db.use('library')  # set database to work
+    # # making a simple query:
+    # result, error = await db.query('SELECT * from events')
+    # print(error)
+    # for row in result:
+    #     print(row)
+    # creation and insertion:
+    await db.execute("CREATE TABLE tests (id int, name text, PRIMARY KEY(id))")
+    ql = "INSERT INTO tests (id, name) VALUES(?, ?)"
+    # prepare the statement:
+    prepared, error = await db.prepare(ql)
+    print(": prepare the statement: ", prepared)
+    print(": Executing Insert of many entries: ")
+    result, error = await db.execute(prepared, (2, "def"))
+    result, error = await db.execute(prepared, (3, "ghi"))
+    result, error = await db.execute(prepared, (4, "jkl"))
+    examples = [(5, "mno"), (6, "pqr"), (7, "stu")]
+    for example in examples:
+        result, error = await db.execute(prepared, example)
+        print(result, error)
+    result, error = await db.query("SELECT * FROM tests")
+    for row in result:
+        print(row)
     await db.close()
 
 
