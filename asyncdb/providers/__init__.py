ccc287749cf5706f8fe5e64b89c3ad6ca526f866
Jesus Lara <jesuslarag@gmail.com>
2022-02-15 02:03:30 +0100

    connector for mcache working.

--- asyncdb/providers/__init__.py
+++ asyncdb/providers/__init__.py
@@ -5,17 +5,24 @@ from asyncdb.utils.functions import module_exists
 from .base import (
     BasePool,
     BaseProvider,
+    InitProvider,
     BaseCursor,
     SQLProvider,
     DDLBackend
 )
 _PROVIDERS = {}
+
 __all__ = [
-    "BasePool", "BaseProvider", "BaseCursor", "SQLProvider", "DDLBackend"
+    "BasePool",
+    "InitProvider",
+    "BaseProvider",
+    "BaseCursor",
+    "SQLProvider",
+    "DDLBackend"
 ]
 
 
-def registerProvider(provider):
+def registerProvider(provider: InitProvider):
     global _PROVIDERS
     name = provider.driver()
     classpath = f"asyncdb.providers.{name}"
