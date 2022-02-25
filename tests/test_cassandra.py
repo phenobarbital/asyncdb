14dba8e8a7d37f257514a9d8eec5bf316ebc57db
Jesus Lara <jesuslarag@gmail.com>
2022-02-25 02:10:54 +0100

    Working version of Cassandra pyTest

--- tests/test_cassandra.py
+++ tests/test_cassandra.py
@@ -11,6 +11,7 @@ def event_loop():
 
 
 DRIVER='cassandra'
+VERSION = '4.0.1'
 PARAMS = {
     "host": "127.0.0.1",
     "port": "9042",
@@ -46,7 +47,7 @@ async def test_connect(driver, event_loop):
     result, error = await db.test_connection()
     pytest.assume(type(result) == list)
     row = result[0]
-    pytest.assume(row['release_version'] == '3.11.8')
+    pytest.assume(row['release_version'] == VERSION)
     await db.close()
     pytest.assume(db.is_connected() is False)
 
