dd309f1244a404910e570761fe93ef6d0d70883f
Jesus Lara <jesuslarag@gmail.com>
2022-09-25 20:49:26 +0200

    WIP: Hazelcast with Portable based on Models.

--- examples/test_hazelcast.py
+++ examples/test_hazelcast.py
@@ -8,7 +8,69 @@ logger = logging.getLogger(__name__)
 class Customer(HazelPortable):
     name: str
     age: int
-    is_active: bool
+
+def on_response(sql_result_future):
+    iterator = sql_result_future.result().iterator()
+
+    def on_next_row(row_future):
+        try:
+            row = row_future.result()
+            # Process the row.
+            print(row)
+
+            # Iterate over the next row.
+            next(iterator).add_done_callback(on_next_row)
+        except StopIteration:
+            # Exhausted the iterator. No more rows are left.
+            pass
+    next(iterator).add_done_callback(on_next_row)
+
+async def test_customer(params, event_loop):
+    c1 = Customer(name="Peter", age=42)
+    c2 = Customer(name="John", age=23)
+    c3 = Customer(name="Joe", age=33)
+    hz = hazel(params=params, loop=event_loop)
+    async with await hz.connection() as conn:
+        print('=============================')
+        print(f"Is Connected: {conn.is_connected()}")
+        # add those customers to Hazelcast:
+        await conn.set(1, c1, map_name='customers')
+        await conn.set(2, c2, map_name='customers')
+        await conn.set(3, c3, map_name='customers')
+        result, error = await conn.query(map_name='customers')
+        print('Result: ', result, 'Error: ', error)
+        result, error = await conn.execute('DROP MAPPING IF EXISTS my_customers')
+        result, error = await conn.execute(
+            """
+            CREATE OR REPLACE MAPPING my_customers (
+                __key INT,
+                name VARCHAR,
+                age INT
+            )
+            TYPE IMap
+            OPTIONS (
+             'keyFormat' = 'int',
+             'valueFormat' = 'portable',
+             'valuePortableFactoryId' = '1',
+             'valuePortableClassId' = '1',
+             'valuePortableClassVersion' = '0'
+            )
+            """
+        )
+        print('MAP: ', result, error)
+        print(result.update_count())
+        print('EXEC QUERY ==== ')
+        result, error = await conn.execute(
+            "SELECT * FROM my_customers WHERE age < 18"
+        )
+        print('RESULT ', result, result.is_row_set())
+        if result.is_row_set():
+            print('METADATA: ', result.get_row_metadata())
+            len(result)
+            for row in result:
+                print('ROW ', row.get_object('name'))
+        # DROPPING MAPPING:
+        result, error = await conn.execute('DROP MAPPING IF EXISTS my_customers')
 
 
 async def test_connect(params, event_loop):
@@ -16,7 +78,7 @@ async def test_connect(params, event_loop):
     async with await hz.connection() as conn:
         print(f"Is Connected: {conn.is_connected()}")
         print('CONN ', conn)
-        await conn.set("Test2", "No More Sweet Music")
+        await conn.put("Test2", "No More Sweet Music")
         value = await conn.get("Test2")
         print(value)
 
@@ -35,6 +97,7 @@ if __name__ == '__main__':
         asyncio.set_event_loop(loop)
         loop.set_debug(True)
         params = {"host": "172.17.0.2", "port": 5701, "factories": [Customer]}
-        loop.run_until_complete(test_connect(params, loop))
+        # loop.run_until_complete(test_connect(params, loop))
+        loop.run_until_complete(test_customer(params, loop))
     finally:
         loop.stop()
