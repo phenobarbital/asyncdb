6af75bf1ab3e7673617b80f2ecdf53ba07bfd96d
Jesus Lara <jesuslarag@gmail.com>
2022-09-29 04:34:48 +0200

    problem with asyncio loop on pg

--- asyncdb/drivers/outputs/__init__.py
+++ asyncdb/drivers/outputs/__init__.py
@@ -3,14 +3,10 @@ from .json import jsonFormat
 from .record import recordFormat
 from .recordset import recordsetFormat
 from .generator import genFormat
-from .datatable import dtFormat
-
 
 __all__ = ['OutputFactory']
-
 
 OutputFactory.register_format('json', jsonFormat)
 OutputFactory.register_format('record', recordFormat)
 OutputFactory.register_format('recordset', recordsetFormat)
 OutputFactory.register_format('iterable', genFormat)
-OutputFactory.register_format('datatable', dtFormat)
