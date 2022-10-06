7ffb4c510c1bdcf1e43a6caacbcddd47fee1b8b1
Jesus Lara <jesuslarag@gmail.com>
2022-10-06 16:21:41 +0200

    New Iterable (basic) output

--- asyncdb/drivers/outputs/__init__.py
+++ asyncdb/drivers/outputs/__init__.py
@@ -3,10 +3,12 @@ from .json import jsonFormat
 from .record import recordFormat
 from .recordset import recordsetFormat
 from .generator import genFormat
+from .iter import iterFormat
 
 __all__ = ['OutputFactory']
 
 OutputFactory.register_format('json', jsonFormat)
 OutputFactory.register_format('record', recordFormat)
 OutputFactory.register_format('recordset', recordsetFormat)
-OutputFactory.register_format('iterable', genFormat)
+OutputFactory.register_format('generator', genFormat)
+OutputFactory.register_format('iterable', iterFormat)
---
+++ asyncdb/drivers/outputs/iter.py
@@ -0,0 +1,18 @@
+"""
+Iterable.
+
+Output format returning a simple list of dictionaries.
+"""
+from .base import OutputFormat
+
+
+class iterFormat(OutputFormat):
+    """
+    Most Basic Definition of Format.
+    """
+    async def serialize(self, result, error, *args, **kwargs):
+        if isinstance(result, list):
+            data = [dict(row) for row in result]
+        else:
+            data = dict(result)
+        return (data, error)
