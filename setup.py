899bb4d0abb23f84ae99c9b97717148ef79cf0bb
Jesus Lara <jesuslarag@gmail.com>
2022-10-17 12:33:29 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -5,9 +5,11 @@
 See:
 https://github.com/phenobarbital/asyncdb
 """
+import ast
 from os import path
-from setuptools import find_packages, setup, Extension
+
 from Cython.Build import cythonize
+from setuptools import Extension, find_packages, setup
 
 
 def get_path(filename):
@@ -19,9 +21,33 @@ def readme():
         return rd.read()
 
 
-with open(get_path('asyncdb/version.py'), encoding='utf-8') as meta:
-    exec(meta.read())
-
+version = get_path('asyncdb/version.py')
+with open(version, 'r', encoding='utf-8') as meta:
+    # exec(meta.read())
+    t = compile(meta.read(), version, 'exec', ast.PyCF_ONLY_AST)
+    for node in (n for n in t.body if isinstance(n, ast.Assign)):
+        if len(node.targets) == 1:
+            name = node.targets[0]
+            if isinstance(name, ast.Name) and \
+                    name.id in (
+                            '__version__',
+                            '__title__',
+                            '__description__',
+                            '__author__',
+                            '__license__', '__author_email__'):
+                        v = node.value
+                        if name.id == '__version__':
+                            __version__ = v.s
+                        if name.id == '__title__':
+                            __title__ = v.s
+                        if name.id == '__description__':
+                            __description__ = v.s
+                        if name.id == '__license__':
+                            __license__ = v.s
+                        if name.id == '__author__':
+                            __author__ = v.s
+                        if name.id == '__author_email__':
+                            __author_email__ = v.s
 
 COMPILE_ARGS = ["-O2"]
 
