5ab0413a93192be852b488701ebc2aa74f3fd3ef
Jesus Lara <jesuslarag@gmail.com>
2022-10-21 01:10:55 +0200

    getting only selected fields from get method on Model

--- asyncdb/__init__.py
+++ asyncdb/__init__.py
@@ -4,10 +4,10 @@
 Asyncio-based database connectors.
 """
 from pathlib import Path
-from .version import (
-    __title__, __description__, __version__, __author__, __author_email__
-)
+
 from .connections import AsyncDB, AsyncPool
+from .version import __author__, __author_email__, __description__, __title__, __version__
+
 
 def get_project_root() -> Path:
     return Path(__file__).parent.parent
