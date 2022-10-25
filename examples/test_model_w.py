6b7fa23d8ce72638dfd4de8b6d36f8fd40c66668
Jesus Lara <jesuslarag@gmail.com>
2022-10-25 19:58:03 +0200

    fixed version of Model insert

--- examples/test_model_w.py
+++ examples/test_model_w.py
@@ -1,10 +1,12 @@
 import asyncio
-from asyncdb import AsyncDB
-from asyncdb.models import Model, Column
 from uuid import UUID
-from typing import Dict
+from typing import Optional
 from datetime import datetime
 from slugify import slugify
+import orjson
+from datamodel.exceptions import ValidationError
+from asyncdb import AsyncDB
+from asyncdb.models import Model, Column
 
 def at_now():
     return datetime.now()
@@ -28,60 +30,76 @@ args = {
     }
 }
 
-
 class WidgetTemplate(Model):
 
-    widget_id: int = Column(required=False, primary_key=True)
+    widget_id: int = Column(required=False, primary_key=True, db_default="auto")
     uid: UUID = Column(required=True, db_default='uuid_generate_v4()')
-    widget_name: str = Column(required=True)
+    widget_name: str = Column(required=False)
     title: str
     description: str
     url: str = Column(required=False)
     active: bool = Column(required=True, default=True)
-    params: Dict = Column(required=False, db_type="jsonb")
+    params: Optional[dict] = Column(required=False, db_type="jsonb")
     embed: str = Column(required=False)
-    attributes: Dict = Column(required=False, db_type="jsonb")
-    conditions: Dict = Column(required=False, db_type="jsonb")
-    cond_definition: Dict = Column(required=False, db_type="jsonb")
-    where_definition: Dict = Column(required=False, db_type="jsonb")
-    format_definition: Dict = Column(required=False, db_type="jsonb")
-    query_slug: Dict = Column(required=False, db_type="jsonb")
-    widget_slug: str = Column(required=True)
+    attributes: Optional[dict] = Column(required=False, db_type="jsonb")
+    conditions: Optional[dict] = Column(required=False, db_type="jsonb")
+    cond_definition: Optional[dict] = Column(required=False, db_type="jsonb")
+    where_definition: Optional[dict] = Column(required=False, db_type="jsonb")
+    format_definition: Optional[dict] = Column(required=False, db_type="jsonb")
+    master_filtering: bool = Column(required=True, default=False)
+    query_slug: Optional[dict] = Column(required=False, db_type="jsonb")
+    widget_slug: str = Column(required=False)
     program_id: int = Column(required=False)
     widget_type_id: str = Column(required=True)
     widgetcat_id: int = Column(required=True)
-    allow_filtering: Dict = Column(required=False, db_type="jsonb")
-    filtering_show: Dict = Column(required=False, db_type="jsonb")
+    allow_filtering: Optional[dict] = Column(required=False, db_type="jsonb")
+    filtering_show: Optional[dict] = Column(required=False, db_type="jsonb")
     inserted_at: datetime = Column(required=False, default=at_now)
+    inserted_by: int = Column(required=False)
+    updated_at: datetime = Column(required=False, default=at_now)
+    updated_by: int = Column(required=False)
 
     def __post_init__(self) -> None:
         if not self.widget_slug:
             self.widget_slug = slugify(self.widget_name, separator='_')
         return super(WidgetTemplate, self).__post_init__()
 
-
-    def __getitem__(self, item):
-        return getattr(self, item)
-
     class Meta:
         driver = "pg"
         name = "template_widgets"
         schema = "troc"
+        strict = True
 
 
-async def create_widget():
+async def work_with_widgets():
     p = AsyncDB("pg", params=params, **args)
     async with await p.connection() as conn:
+        WidgetTemplate.Meta.connection = conn
+        try:
+            tpl = await WidgetTemplate.get(uid='b13b619a-847e-4734-a3d2-fa198f0531b7')
+        except ValidationError as ex:
+            print(ex.payload)
+        print(tpl)
+        # TO JSON:
+        print('=== TO JSON ===')
+        print(tpl.json(option=orjson.OPT_INDENT_2))
         data = {
             "widget_type_id": "api-card",
             "widget_name": "Nuevo widget Template",
             # "widget_slug": "iframe_new",
             "widgetcat_id": 3
         }
-        tpl = WidgetTemplate(**data)
-        tpl.set_connection(conn)
-        print(tpl)
-        await tpl.insert()
+        try:
+            tpl = WidgetTemplate(**data)
+            print(tpl)
+        except ValidationError as ex:
+            print(ex.payload)
+        print(':: CREATED NEW ::')
+        result = await tpl.insert()
+        print('=== RESULT == ')
+        print(result)
+        print('=== TO JSON ===')
+        print(result.json(option=orjson.OPT_INDENT_2))
 
 if __name__ == '__main__':
-    loop.run_until_complete(create_widget())
+    loop.run_until_complete(work_with_widgets())
