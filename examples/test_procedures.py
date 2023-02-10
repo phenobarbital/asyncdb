4ba76b04c19255be04cbd674bbc3e5062172dd67
Jesus Lara <jesuslarag@gmail.com>
2023-02-10 12:05:49 +0100

    removed old requirements

--- examples/test_procedures.py
+++ examples/test_procedures.py
@@ -5,26 +5,48 @@ from asyncdb.exceptions import default_exception_handler
 
 params = {
     "host": "localhost",
-    "port": "1433",
-    "database": 'AdventureWorks2019',
-    "user": 'SA',
-    "password": 'P4ssW0rd1.'
+    "port": "3307",
+    "database": 'PRD_MI',
+    "user": 'navuser',
+    "password": 'L2MeomsUgYpFBJ6t'
 }
 
 DRIVER = 'sqlserver'
 
 
-async def connect(db):
+async def form_data(db):
     async with await db.connection() as conn:
         print('Getting Driver: ', conn)
         pprint(await conn.test_connection())
         pprint(conn.is_connected())
-        args = {"@OrgId": 73, "@UserId": 1, "@PageSize": 100, "@StartIndex": 1}
+        args = {"@OrgId": 69, "@UserId": 1, "@FormId": 2662, "@StartDateTime": "'2022-06-17 00:00:00'", "@EndDateTime": "'2022-06-17 23:59:59'"}
         result, error = await conn.exec(
-            "GET_PRODUCTS_LIST", **args, paginated=True, page="TotalRowCount", idx="@StartIndex"
+            "FORM_DATA_VIEW_GET_LIST", **args, paginated=False, page="TotalRowCount", idx="@StartIndex"
         )
-        print(result, len(result), error)
+        print(result, error)
+        # print(result, len(result), error)
 
+
+async def activities(db):
+    async with await db.connection() as conn:
+        print('Getting Driver: ', conn)
+        pprint(await conn.test_connection())
+        pprint(conn.is_connected())
+        args = {
+            "@OrgId": 74,
+            "@UserId": 1,
+            "@StartDateFrom": "'2022-06-17 00:00:00'",
+            "@EndDateFrom": "'2022-06-17 23:59:59'",
+            "@includeSubmittedActivities": 1,
+            "@PageSize": 1000
+        }
+        result, error = await conn.exec(
+            "dbo.ACTIVITY_ITEM_GET_LIST", **args, paginated=True, page="TotalRowCount", idx="@StartIndex"
+        )
+        print(error)
+        if result:
+            print(len(result))
+        # print(result, len(result), error)
 
 if __name__ == "__main__":
     try:
@@ -32,6 +54,6 @@ if __name__ == "__main__":
         loop.set_exception_handler(default_exception_handler)
         driver = AsyncDB(DRIVER, params=params, loop=loop)
         print(driver)
-        loop.run_until_complete(connect(driver))
+        loop.run_until_complete(activities(driver))
     finally:
         loop.stop()
