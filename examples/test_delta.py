706439af1e8cdae67de92530fcab3097b37bcbc0
Jesus Lara <jesuslarag@gmail.com>
2023-03-24 21:32:58 +0100

    WIP: support for deltatables

--- examples/test_delta.py
+++ examples/test_delta.py
@@ -30,15 +30,63 @@ async def test_data(evt: asyncio.AbstractEventLoop):
         )
         print(result, error)
 
+async def test_epson(evt: asyncio.AbstractEventLoop):
+    args = {
+        "pd_args": {
+            "sep": "|",
+            "header": False,
+            "columns": [
+                "week_end_date",
+                "customer_id",
+                "customer_description",
+                "store_number",
+                "product_type",
+                "model",
+                "model_description",
+                "sale_class",
+                "sale_subclass",
+                "material",
+                "material_description",
+                "sale_qty",
+                "return_qty",
+                "sell_thru_qty",
+                "sell_thru_rev",
+                "on_hand_qty"
+            ],
+        }
+    }
+    params = {
+        "filename": "docs/epson.sales/"
+    }
+    dt = delta(params=params, loop=evt)
+    filename = '/home/ubuntu/symbits/epson/files/sales/SELLTHRU_TROC_20200630_091802.TXT'
+    parquet_dir = Path(__file__).parent.parent.joinpath('docs', "epson.sales")
+    parquet = parquet_dir.joinpath("20200630_091802.parquet")
+    await dt.file_to_parquet(filename, parquet, factory='datatable', **args)
+    await dt.create(parquet_dir, parquet, name='EPSON_SALES', mode='overwrite')
+    async with await dt.connection() as conn:
+        # querying data:
+        result, error = await conn.query(
+            columns=['customer_id', 'week_end_date', 'store_number', 'product_type', 'model'],
+            factory='pandas'
+        )
+        print('ERROR > ', error)
+        group = result.groupby(['customer_id']).count()
+        print(group)
+        print(result)
+
 if __name__ == '__main__':
     start_time = time.time()  # Record the start time
     loop = asyncio.new_event_loop()
     loop.set_debug(True)
+    # loop.run_until_complete(
+    #     test_connection(evt=loop)
+    # )
+    # loop.run_until_complete(
+    #     test_data(evt=loop)
+    # )
     loop.run_until_complete(
-        test_connection(evt=loop)
-    )
-    loop.run_until_complete(
-        test_data(evt=loop)
+        test_epson(evt=loop)
     )
     end_time = time.time()  # Record the end time
     duration_seconds = end_time - start_time
