1ee2cf07f9513af8e297f5b1b559889c5cdf4eb4
Jesus Lara <jesuslarag@gmail.com>
2023-09-13 20:12:08 +0200

    new version of aiohttp

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
     install_requires=[
         "numpy==1.24.2",
         "cryptography==37.0.4",
-        "aiohttp==3.8.4",
+        "aiohttp==3.8.5",
         "asyncpg==0.27.0",
         "uvloop==0.17.0",
         "asyncio==3.4.3",
