baaa7e63c6bda51655a5081ba07b5eb342316788
Jesus Lara <jesuslarag@gmail.com>
2023-12-06 17:56:25 +0100

    tested asyncioConnection on experimental driver for Cassandra

--- asyncdb/drivers/cassandra.py
+++ asyncdb/drivers/cassandra.py
@@ -17,6 +17,8 @@ import pandas as pd
 from cassandra import ReadTimeout
 from cassandra.cluster import Cluster, EXEC_PROFILE_DEFAULT, ExecutionProfile, NoHostAvailable, ResultSet
 from cassandra.io.asyncorereactor import AsyncoreConnection
+from cassandra.io.asyncioreactor import AsyncioConnection
+
 try:
     from cassandra.io.libevreactor import LibevConnection
     LIBEV = True
