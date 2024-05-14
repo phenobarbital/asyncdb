718a09c756ff89043e83f716eced2e181a51688a
Jesus Lara <jesuslarag@gmail.com>
2024-05-14 17:51:21 +0200

    optimal parquet file

--- examples/test_delta.py
+++ examples/test_delta.py
@@ -44,49 +44,55 @@ async def test_data(evt: asyncio.AbstractEventLoop):
         print(result, error)
 
 async def test_epson(evt: asyncio.AbstractEventLoop):
+    # Convert a file to parquet:
     args = {
-        "pd_args": {
-            "sep": "|",
-            "header": False,
-            "columns": [
-                "week_end_date",
-                "customer_id",
-                "customer_description",
-                "store_number",
-                "product_type",
-                "model",
-                "model_description",
-                "sale_class",
-                "sale_subclass",
-                "material",
-                "material_description",
-                "sale_qty",
-                "return_qty",
-                "sell_thru_qty",
-                "sell_thru_rev",
-                "on_hand_qty"
-            ],
-        }
+        "has_header": False,
+        "replace_destination": True,
+        "columns": [
+            "week_end_date",
+            "customer_id",
+            "customer_description",
+            "store_number",
+            "product_type",
+            "model",
+            "model_description",
+            "sale_class",
+            "sale_subclass",
+            "material",
+            "material_description",
+            "sale_qty",
+            "return_qty",
+            "sell_thru_qty",
+            "sell_thru_rev",
+            "on_hand_qty"
+        ],
     }
     params = {
         "path": "docs/epson.sales/"
     }
     dt = delta(params=params, loop=evt)
-    filename = '/home/ubuntu/symbits/epson/files/sales/SELLTHRU_TROC_2020.TXT'
+    source = Path('/home/ubuntu/symbits/epson/files/sales/SELLTHRU_TROC_2020.TXT')
     parquet_dir = Path(__file__).parent.parent.joinpath('docs', "epson.sales")
-    parquet = parquet_dir.joinpath("20200630_091802.parquet")
-    await dt.file_to_parquet(filename, parquet, factory='datatable', **args)
-    await dt.create(parquet_dir, parquet, name='EPSON_SALES', mode='overwrite')
-    async with await dt.connection() as conn:
-        # querying data:
-        result, error = await conn.query(
-            sentence="passenger_count > 2",
-            factory='pandas'
-        )
-        print('ERROR > ',result, error)
-        group = result.groupby(['customer_id']).count()
-        print(group)
-        print(result)
+    destination = parquet_dir.joinpath("20200630_091802.parquet")
+    parquet, meta = await dt.copy_to(
+        source=source,
+        destination=destination,
+        separator='|',
+        lazy=True,
+        **args
+    )
+    print('RESULT > ', parquet, meta)
+    # await dt.create(parquet_dir, parquet, name='EPSON_SALES', mode='overwrite')
+    # async with await dt.connection() as conn:
+    #     # querying data:
+    #     result, error = await conn.query(
+    #         sentence="passenger_count > 2",
+    #         factory='pandas'
+    #     )
+    #     print('ERROR > ',result, error)
+    #     group = result.groupby(['customer_id']).count()
+    #     print(group)
+    #     print(result)
 
 if __name__ == '__main__':
     start_time = time.time()  # Record the start time
@@ -95,12 +101,14 @@ if __name__ == '__main__':
     # loop.run_until_complete(
     #     test_connection(evt=loop)
     # )
-    loop.run_until_complete(
-        test_data(evt=loop)
-    )
     # loop.run_until_complete(
-    #     test_epson(evt=loop)
+    #     test_data(evt=loop)
     # )
+    loop.run_until_complete(
+        test_epson(evt=loop)
+    )
     end_time = time.time()  # Record the end time
     duration_seconds = end_time - start_time
-    print(f"Duration: {duration_seconds} seconds")
+    print(
+        f"Duration: {duration_seconds} seconds"
+    )
