eb1b799ed9f7810248b8c0cb1e0d0817c42d0767
Jesus Lara <jesuslarag@gmail.com>
2024-07-03 01:32:30 +0200

    fix on _insert_ method of model

--- asyncdb/version.py
+++ asyncdb/version.py
@@ -3,7 +3,7 @@
 __title__ = "asyncdb"
 __description__ = "Library for Asynchronous data source connections \
     Collection of asyncio drivers."
-__version__ = "2.7.11"
+__version__ = "2.7.12"
 __author__ = "Jesus Lara"
 __author_email__ = "jesuslarag@gmail.com"
 __license__ = "BSD"
