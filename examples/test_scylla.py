75304aa7430460199e14b1c5bbc82c5de11ba861
Jesus Lara <jesuslarag@gmail.com>
2024-07-08 19:51:33 +0200

    wip: bigqueru model

--- examples/test_scylla.py
+++ examples/test_scylla.py
@@ -11,8 +11,20 @@ ARGS = {
     "password": 'navigator'
 }
 
+ARGS = {
+    "host": "10.10.10.93",
+    "port": "9042",
+    "username": '',
+    "password": ''
+}
+
 async def test_connection():
-    db = AsyncDB(DRIVER, params=ARGS)
+    db = AsyncDB(
+        DRIVER,
+        params=ARGS,
+        shard_awareness=True,
+        whitelist=["10.10.10.93"]
+    )
     async with await db.connection() as conn:
         print('CONNECTED: ', conn.is_connected() is True)
         result, error = await conn.test_connection()
@@ -21,7 +33,12 @@ async def test_connection():
 
 async def test_async():
     ARGS['driver'] = 'async'
-    db = AsyncDB(DRIVER, params=ARGS)
+    db = AsyncDB(
+        DRIVER,
+        params=ARGS,
+        shard_awareness=True,
+        whitelist=["10.10.10.93"]
+    )
     async with await db.connection() as conn:
         print('CONNECTED: ', conn.is_connected() is True)
         result, error = await conn.test_connection()
@@ -29,71 +46,76 @@ async def test_async():
         print(type(result) == list)
 
 async def test_operations():
-    db = AsyncDB(DRIVER, params=ARGS)
-    await db.connection()
-    print('CONNECTED: ', db.is_connected() is True)
-    result, error = await db.test_connection()
-    print(result, error)
-    print(type(result) == list)
-    await db.create_keyspace('navigator')
-    await db.use('navigator')  # set database to work
-    # creation and insertion:
-    result, error = await db.execute(
-        "CREATE TABLE IF NOT EXISTS tests (id int, name text, PRIMARY KEY(id))"
+    db = AsyncDB(
+        DRIVER,
+        params=ARGS,
+        shard_awareness=True,
+        whitelist=["10.10.10.93"]
     )
-    print('CREATE > ', result, error)
-    ql = "INSERT INTO tests (id, name) VALUES(?, ?)"
-    # prepare the statement:
-    prepared = await db.prepare(ql)
-    print(": prepared statement: ", prepared)
-    print(": Executing Insert of many entries: ")
-    result, error = await db.execute(prepared, (1, "abc"))
-    result, error = await db.execute(prepared, (2, "def"))
-    result, error = await db.execute(prepared, (3, "ghi"))
-    result, error = await db.execute(prepared, (4, "jkl"))
-    print('Execute a Many insert:: ')
-    examples = [(i, ''.join(random.choices(string.ascii_lowercase, k=4))) for i in range(5, 10005)]
-    result, error = await db.execute_many(prepared, examples)
-    print(result, error)
-    print('first select:')
-    result, error = await db.query("SELECT * FROM tests")
-    for row in result:
-        print(row)
-    # getting column info from Table:
-    columns = await db.column_info(table='tests', schema='navigator')
-    print('COLUMNS ', columns)
-    # using row factories
-    result, error = await db.query('SELECT * from tests LIMIT 10000', factory='pandas')
-    print(result.result)
-    result, error = await db.query('SELECT * from tests LIMIT 10000', factory='recordset')
-    print(result.result)
-    # output formats
-    db.output_format('json')  # change output format to json
-    result, error = await db.query('SELECT * from tests LIMIT 10000')
-    print(result)
+    async with await db.connection() as conn:
+        print('CONNECTED: ', conn.is_connected() is True)
+        result, error = await conn.test_connection()
+        print(result, error)
+        print(type(result) == list)
+        await conn.create_keyspace('navigator')
+        await conn.use('navigator')  # set database to work
+        # creation and insertion:
+        result, error = await db.execute(
+            "CREATE TABLE IF NOT EXISTS tests (id int, name text, PRIMARY KEY(id))"
+        )
+        print('CREATE > ', result, error)
+        ql = "INSERT INTO tests (id, name) VALUES(?, ?)"
+        # prepare the statement:
+        prepared = await conn.prepare(ql)
+        print(": prepared statement: ", prepared)
+        print(": Executing Insert of many entries: ")
+        result, error = await conn.execute(prepared, (1, "abc"))
+        result, error = await conn.execute(prepared, (2, "def"))
+        result, error = await conn.execute(prepared, (3, "ghi"))
+        result, error = await conn.execute(prepared, (4, "jkl"))
+        print('Execute a Many insert:: ')
+        examples = [(i, ''.join(random.choices(string.ascii_lowercase, k=4))) for i in range(5, 10005)]
+        result, error = await conn.execute_many(prepared, examples)
+        print(result, error)
+        print('first select:')
+        result, error = await conn.query("SELECT * FROM tests")
+        for row in result:
+            print(row)
+        # getting column info from Table:
+        columns = await db.column_info(table='tests', schema='navigator')
+        print('COLUMNS ', columns)
+        # using row factories
+        result, error = await conn.query('SELECT * from tests LIMIT 10000', factory='pandas')
+        print(result.result)
+        result, error = await conn.query('SELECT * from tests LIMIT 10000', factory='recordset')
+        print(result.result)
+        # output formats
+        conn.output_format('json')  # change output format to json
+        result, error = await conn.query('SELECT * from tests LIMIT 10000')
+        print(result)
 
-    db.output_format('pandas')  # change output format to pandas
-    result, error = await db.query('SELECT * from tests LIMIT 10000')
-    print(result)
-    db.output_format('polars')  # change output format to iter generator
-    result, error = await db.query('SELECT * from tests LIMIT 10000')
-    print(result)
-    # change output format to iter generator
-    db.output_format('dt')
-    # TODO: error when a python list is on a column
-    result, error = await db.query('SELECT * from tests LIMIT 10000')
-    print(result)
-    print(type(result))
-    db.output_format('record')   # change output format to iter generator
-    result, error = await db.query('SELECT * from tests LIMIT 10000')
-    print(type(result))
-    # testing Recordset Object
-    db.output_format('recordset')  # change output format to ResultSet
-    result, error = await db.query('SELECT * from tests LIMIT 10000')
-    for row in result:
-        print(row)
-    await db.execute('DROP TABLE tests')
-    await db.close()
+        conn.output_format('pandas')  # change output format to pandas
+        result, error = await conn.query('SELECT * from tests LIMIT 10000')
+        print(result)
+        conn.output_format('polars')  # change output format to iter generator
+        result, error = await conn.query('SELECT * from tests LIMIT 10000')
+        print(result)
+        # change output format to iter generator
+        conn.output_format('dt')
+        # TODO: error when a python list is on a column
+        result, error = await conn.query('SELECT * from tests LIMIT 10000')
+        print(result)
+        print(type(result))
+        conn.output_format('record')   # change output format to iter generator
+        result, error = await conn.query('SELECT * from tests LIMIT 10000')
+        print(type(result))
+        # testing Recordset Object
+        conn.output_format('recordset')  # change output format to ResultSet
+        result, error = await conn.query('SELECT * from tests LIMIT 10000')
+        for row in result:
+            print(row)
+        await conn.execute('DROP TABLE tests')
+        await conn.close()
 
 
 if __name__ == '__main__':
